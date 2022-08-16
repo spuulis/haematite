9ba4a7a667e96c4d78f4d93bdee1ab6221ce2ee0
Viesturs Spūlis <v.spuulis@gmail.com>
2022-08-16 12:40:25 +0300

    Updated config.py with calibrated coil values

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ CHESSBOARD_SIZE = 10e-3     # Side length of one square in meters (SI)
 
 # Coil parameters
 COILS_MAX_CURRENT = 5.
-COILS_FPS = 5
+COILS_FPS = 60
 COILS_NAME_OX = 'Dev/ao1'
 COILS_NAME_OY = 'Dev/ao0'
 COILS_NAME_IX = 'Dev/ai1'
 COILS_NAME_IY = 'Dev/ai0'
-COILS_MT_TO_V_X = 0.608
-COILS_MT_TO_V_Y = 0.608
+COILS_MT_TO_V_X = 0.6296296296
+COILS_MT_TO_V_Y = 0.6497725796
 
 # Marker parameters
 MARKER_SIZE = 5.08e-3       # Side length of marker in meters (SI)
