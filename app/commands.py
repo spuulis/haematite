63475c185c6db51dce5c8e4d898b66de3412c30b
DanielsGor <91071028+DanielsGor@users.noreply.github.com>
2022-08-16 15:52:57 +0300

    stuf

--- app/commands.py
+++ app/commands.py
@@ -1,7 +1,23 @@
 import tkinter as tk
+import numpy as np
 
-def btn_test(label):
+def fnc_lblUpdate(label):
     n = label["text"]
     label["text"] = str(int(n)+1)
 
     label.update()
+
+def check_num(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+def get_values(amp, freq, phase):
+    if(check_num(amp) and check_num(freq) and check_num(phase)):
+        x = np.linspace(0, 5,1001)
+        y = float(amp) * np.sin(x*float(freq)*2*np.pi+float(phase))
+        return(x,y)
+    else:
+        return([1],[1])
--- app/gui.py
+++ app/gui.py
@@ -1,40 +1,121 @@
 import tkinter as tk
 import commands as cmnd
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
+NavigationToolbar2Tk)
 # from visual.calibration import chessboard
 # import visual.tracker
  
 
 window = tk.Tk()
-# window.geometry("800x800")
-window.configure(background = "grey")
+window.geometry("800x800")
+# window.configure(background = "grey")
 window.title("Pagraba spoles")
-window.resizable(True, True)
+window.resizable(False, False)
 
-frame = tk.Frame(
+frm_labels = tk.Frame(
     master=window,
-    relief=tk.RAISED,
-    borderwidth=5
+    borderwidth=0
 )
 
 
-label = tk.Label(
-    master=frame,
-    text="0"
+lbl_freq = tk.Label(
+    master=frm_labels,
+    text="Frequency:",
+    anchor="e",
+    width=10
+)
+lbl_amp = tk.Label(
+    master=frm_labels,
+    text="Amplitude:",
+    anchor="e",
+    width=10
+)
+lbl_phase = tk.Label(
+    master=frm_labels,
+    text="Phase:",
+    anchor="e",
+    width=10
 )
 
-button = tk.Button(
-    master=frame,
-    text="count up",
-    width=10,
-    height=2,
-    bg="#fafafa",
-    command=lambda: cmnd.btn_test(label)
+
+ent_freq = tk.Entry(
+    master=frm_labels,
+    width=10
+)
+ent_amp = tk.Entry(
+    master=frm_labels,
+    width=10
+)
+ent_phase = tk.Entry(
+    master=frm_labels,
+    width=10
 )
 
-button.pack()
-label.pack()
 
-window.update_idletasks()
+frm_labels.place(x=5,y=5)
+lbl_freq.grid(row=0,column=0)
+lbl_amp.grid(row=1,column=0)
+lbl_phase.grid(row=2,column=0)
 
-frame.place(x=20, y=20)
+ent_freq.grid(row=0,column=1)
+ent_amp.grid(row=1,column=1)
+ent_phase.grid(row=2,column=1)
+frm_labels.update()
+
+# lbl_amp = tk.Label(
+#     master=window,
+#     text="amplitude:"
+# )
+
+btn_plot = tk.Button(
+    master=window,
+    text="Plot sine",
+    width=20,
+    height=1,
+    bg="#aafaaa",
+    command=lambda: plot()
+)
+
+
+btn_plot.place(x=5, y=frm_labels.winfo_height()+10)
+btn_plot.update()
+
+
+
+b_canvas = tk.Canvas(
+    master=window,
+    height=300,
+    width=300,
+).place(x=20,y=120)
+def plot():
+
+    fig = Figure(figsize = (10, 10), dpi = 50)        
+    x,y = cmnd.get_values(ent_amp.get(),ent_freq.get(),ent_phase.get())
+    plot1 = fig.add_subplot(111)
+    plot1.plot(x,y)
+
+    canvas = FigureCanvasTkAgg(
+        fig,
+        master = b_canvas
+    )  
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+  
+    toolbar = NavigationToolbar2Tk(
+        canvas,
+        b_canvas
+    )
+    toolbar.update()
+
+    canvas.get_tk_widget().place(x=0,y=0)
+# button.pack()
+# label.pack()
+
+# window.update_idletasks()
+
+# frame.place(x=20, y=20)
+print(btn_plot.winfo_rooty())
 window.mainloop()
