20b23f5fc0c8567e9a97c149030cb11cea18178d
Viesturs Spūlis <v.spuulis@gmail.com>
2022-08-16 12:28:25 +0300

    Added a class to generate a waveform and control coils

--- config.py
+++ config.py
@@ -4,6 +4,16 @@ import cv2
 CHESSBOARD_DIM = (6, 5)     # Number of corners (width, height)
 CHESSBOARD_SIZE = 10e-3     # Side length of one square in meters (SI)
 
+# Coil parameters
+COILS_MAX_CURRENT = 5.
+COILS_FPS = 5
+COILS_NAME_OX = 'Dev/ao1'
+COILS_NAME_OY = 'Dev/ao0'
+COILS_NAME_IX = 'Dev/ai1'
+COILS_NAME_IY = 'Dev/ai0'
+COILS_MT_TO_V_X = 0.608
+COILS_MT_TO_V_Y = 0.608
+
 # Marker parameters
 MARKER_SIZE = 5.08e-3       # Side length of marker in meters (SI)
 MARKER_IS_INVERTED = True   # Is the marker black on white
