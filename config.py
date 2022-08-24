e1bffea5f1e9c605db80776c2352213fa509e41e
Viesturs Spūlis <v.spuulis@gmail.com>
2022-08-24 17:10:28 +0300

    Added function to estimate cube poses

--- config.py
+++ config.py
@@ -20,3 +20,63 @@ COILS_T_TO_V_Y = 649.7725796
 MARKER_SIZE = 5.08e-3       # Side length of marker in meters (SI)
 MARKER_IS_INVERTED = True   # Is the marker black on white
 MARKER_DICT = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
+
+MARKER_DETECTION_PARAMS = cv2.aruco.DetectorParameters_create()
+MARKER_DETECTION_PARAMS.detectInvertedMarker = True
+# Thresholding
+MARKER_DETECTION_PARAMS.adaptiveThreshWinSizeMin = 35
+MARKER_DETECTION_PARAMS.adaptiveThreshWinSizeMax = 85
+MARKER_DETECTION_PARAMS.adaptiveThreshWinSizeStep = 10
+# Contour filtering
+MARKER_DETECTION_PARAMS.minMarkerPerimeterRate = 0.08
+MARKER_DETECTION_PARAMS.maxMarkerPerimeterRate = 0.32
+MARKER_DETECTION_PARAMS.polygonalApproxAccuracyRate = 0.05
+MARKER_DETECTION_PARAMS.minCornerDistanceRate = 0.05
+MARKER_DETECTION_PARAMS.minMarkerDistanceRate = 0.05
+# Corner refinement
+MARKER_DETECTION_PARAMS.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_SUBPIX
+MARKER_DETECTION_PARAMS.cornerRefinementWinSize = 5
+MARKER_DETECTION_PARAMS.cornerRefinementMaxIterations = 30
+MARKER_DETECTION_PARAMS.cornerRefinementMinAccuracy = 0.001
+
+
+# Cube parameters
+CUBE_SIZE = 9.9e-3
+CUBE_MARKER_POSITIONS = [
+    [   # Marker 0
+        [MARKER_SIZE / 2, MARKER_SIZE / 2, -CUBE_SIZE / 2],
+        [-MARKER_SIZE / 2, MARKER_SIZE / 2, -CUBE_SIZE / 2],
+        [-MARKER_SIZE / 2, -MARKER_SIZE / 2, -CUBE_SIZE / 2],
+        [MARKER_SIZE / 2, -MARKER_SIZE / 2, -CUBE_SIZE / 2],
+    ],
+    [   # Marker 1
+        [-MARKER_SIZE / 2, -CUBE_SIZE / 2, MARKER_SIZE / 2],
+        [MARKER_SIZE / 2, -CUBE_SIZE / 2, MARKER_SIZE / 2],
+        [MARKER_SIZE / 2, -CUBE_SIZE / 2, -MARKER_SIZE / 2],
+        [-MARKER_SIZE / 2, -CUBE_SIZE / 2, -MARKER_SIZE / 2],
+    ],
+    [   # Marker 2
+        [-CUBE_SIZE / 2, MARKER_SIZE / 2, -MARKER_SIZE / 2],
+        [-CUBE_SIZE / 2, MARKER_SIZE / 2, MARKER_SIZE / 2],
+        [-CUBE_SIZE / 2, -MARKER_SIZE / 2, MARKER_SIZE / 2],
+        [-CUBE_SIZE / 2, -MARKER_SIZE / 2, -MARKER_SIZE / 2],
+    ],
+    [   # Marker 3
+        [CUBE_SIZE / 2, MARKER_SIZE / 2, MARKER_SIZE / 2],
+        [CUBE_SIZE / 2, MARKER_SIZE / 2, -MARKER_SIZE / 2],
+        [CUBE_SIZE / 2, -MARKER_SIZE / 2, -MARKER_SIZE / 2],
+        [CUBE_SIZE / 2, -MARKER_SIZE / 2, MARKER_SIZE / 2],
+    ],
+    [   # Marker 4
+        [-MARKER_SIZE / 2, CUBE_SIZE / 2, -MARKER_SIZE / 2],
+        [MARKER_SIZE / 2, CUBE_SIZE / 2, -MARKER_SIZE / 2],
+        [MARKER_SIZE / 2, CUBE_SIZE / 2, MARKER_SIZE / 2],
+        [-MARKER_SIZE / 2, CUBE_SIZE / 2, MARKER_SIZE / 2],
+    ],
+    [   # Marker 5
+        [-MARKER_SIZE / 2, MARKER_SIZE / 2, CUBE_SIZE / 2],
+        [MARKER_SIZE / 2, MARKER_SIZE / 2, CUBE_SIZE / 2],
+        [MARKER_SIZE / 2, -MARKER_SIZE / 2, CUBE_SIZE / 2],
+        [-MARKER_SIZE / 2, -MARKER_SIZE / 2, CUBE_SIZE / 2],
+    ],
+]
--- visual/tracker.py
+++ visual/tracker.py
@@ -2,72 +2,91 @@ import cv2
 import numpy as np
 from scipy.spatial.transform import Rotation
 
-aruco_params = cv2.aruco.DetectorParameters_create()
-# Thresholding
-aruco_params.adaptiveThreshWinSizeMin = 35
-aruco_params.adaptiveThreshWinSizeMax = 85
-aruco_params.adaptiveThreshWinSizeStep = 10
-# Contour filtering
-aruco_params.minMarkerPerimeterRate = 0.08
-aruco_params.maxMarkerPerimeterRate = 0.32
-aruco_params.polygonalApproxAccuracyRate = 0.05
-aruco_params.minCornerDistanceRate = 0.05
-aruco_params.minMarkerDistanceRate = 0.05
-# Corner refinement
-aruco_params.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_SUBPIX
-aruco_params.cornerRefinementWinSize = 5
-aruco_params.cornerRefinementMaxIterations = 30
-aruco_params.cornerRefinementMinAccuracy = 0.001
 
-
-def find_markers(img, aruco_dict, inverted=False):
-    aruco_params.detectInvertedMarker = inverted
+def find_markers(img, aruco_dict, aruco_params):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    return cv2.aruco.detectMarkers(gray, aruco_dict, parameters=aruco_params)
+    (corners, ids, _) = cv2.aruco.detectMarkers(
+        gray, aruco_dict, parameters=aruco_params)
+    return [
+        {'id': id_[0], 'corners': corners_}
+        for id_, corners_ in zip(ids, corners)
+    ]
 
 
 def pose_markers(
-        mtx, dist, corners, ids, marker_size,
-        euler='zyx', all_solutions=False):
-    markers = []
+        mtx, dist, markers, marker_size, all_solutions=False):
+    poses = []
     shape = np.array([
         [-.5, .5, 0.], [.5, .5, 0.],
         [.5, -.5, 0.], [-.5, -.5, 0.],
     ]) * marker_size
 
-    for i in range(len(corners)):
+    for marker in markers:
         # Estimate the pose of markers
         n_sol, rvecs, tvecs, err = cv2.solvePnPGeneric(
-            shape, corners[i], mtx, dist,
+            shape, marker['corners'], mtx, dist,
             flags=cv2.SOLVEPNP_IPPE_SQUARE,
         )
 
         if not all_solutions:
             n_sol = 1
-
         # Go through all (both) returned solutions
         for j in range(n_sol):
-            # Format rotation angles as Euler angles
-            r_matrix = cv2.Rodrigues(rvecs[j].reshape(3))[0]
-            r = Rotation.from_matrix(r_matrix)
-            [rx, ry, rz] = r.as_euler(euler, degrees=True)
-
             # Save data
-            markers.append({
-                'id': ids[i][0],
-                'tvecs_x': tvecs[j].reshape(3)[0],
-                'tvecs_y': tvecs[j].reshape(3)[1],
-                'tvecs_z': tvecs[j].reshape(3)[2],
-                'rvecs_x': rvecs[j].reshape(3)[0],
-                'rvecs_y': rvecs[j].reshape(3)[1],
-                'rvecs_z': rvecs[j].reshape(3)[2],
-                'reulr_x': rx,
-                'reulr_y': ry,
-                'reulr_z': rz,
+            poses.append({
+                'id': marker['id'],
+                'tvec': tvecs[j].reshape(3),
+                'rvec': rvecs[j].reshape(3),
                 'rep_err': err[j][0],
             })
 
-    return markers
+    return poses
+
+
+def pose_cubes(mtx, dist, markers, marker_positions):
+    poses = []
+    # Find which cubes have at least one marker visible
+    cube_ids = np.unique([marker['id'] // 6 for marker in markers])
+    for cube_id in cube_ids:
+        # Find markers belonging to the cube
+        cube_markers = [
+            marker for marker in markers if marker['id'] // 6 is cube_id
+        ]
+
+        # Match points on the cube and the image
+        objpoints = []
+        imgpoints = []
+        for marker in cube_markers:
+            objpoints = np.concatenate((
+                objpoints,
+                marker_positions[marker['id'] % 6],
+            ))
+            imgpoints = np.concatenate((
+                imgpoints,
+                marker['corners'],
+            ))
+        
+        # Estimate and save pose
+        rvec, tvec = cv2.solvePnP(
+            objpoints, imgpoints, mtx, dist, flags=cv2.SOLVEPNP_EPNP
+        )
+        poses.append({
+            'id': cube_id,
+            'rvec': rvec,
+            'tvec': tvec,
+        })
+
+    return poses
+
+
+def camera_to_aboslute_ref_frame(tvec, rvec, repos, euler="zyx"):
+    pass
+
+
+def rvec_to_relr(rvec, euler="zyx"):
+    r_matrix = cv2.Rodrigues(rvec.reshape(3))[0]
+    r = Rotation.from_matrix(r_matrix)
+    return r.as_euler(euler, degrees=True)
 
 
 def draw_marker_axis(mtx, dist, img, markers, axis_length):
