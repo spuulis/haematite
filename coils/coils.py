99ca5df1cbab99a62011e53e9a0338adde8d89ba
Viesturs Spūlis <v.spuulis@gmail.com>
2022-08-26 16:13:23 +0300

    Coils and camera can now be manipulated during use

--- coils/coils.py
+++ coils/coils.py
@@ -1,5 +1,4 @@
 from contextlib import nullcontext
-import csv
 import time
 
 import nidaqmx
@@ -21,67 +20,79 @@ def turn_off():
 
 
 class Coils(threading.Thread):
-    def __init__(self, amp, freq):
+    def __init__(self, amp=0., freq=0.):
         threading.Thread.__init__(self)
         self.daemon = True
 
-        self.amp = amp
-        self.freq = freq
-        self.phase = np.pi / 2
+        self.params = {
+            'x_amp': amp,
+            'y_amp': amp,
+            'x_freq': freq,
+            'y_freq': freq,
+            'x_phase': 0.,
+            'y_phase': np.pi / 2,
+        }
 
-        # File name for csv output
-        self.output = None
+        self._field = {'x': 0, 'y': 0}
+        self.initialized = False
 
         self._stopper = threading.Event()
 
-    def stopit(self):
+    def update_params(self, new_params):
+        self.params.update(new_params)
+
+    def stop(self):
         self._stopper.set()
 
-    def stopped(self):
-        return self._stopper.is_set()
+    def initialize(self):
+        try:
+            with nidaqmx.Task() as task_o:
+                task_o = nidaqmx.Task()
+                # Define voltage output channels for coil control ([X, Y])
+                task_o.ao_channels.add_ao_voltage_chan(config.COILS_NAME_OX)
+                task_o.ao_channels.add_ao_voltage_chan(config.COILS_NAME_OY)
+        except Exception:
+            raise Exception("Coils could not be initialized")
+        else:
+            self.initialized = True
 
-    def set_output_file(self, file_name):
-        self.output = file_name
+    @property
+    def field(self):
+        return self._field.copy()
 
     def run(self):
-        with (
-            nidaqmx.Task() as task_o,
-            open(self.output, 'w') if self.output is not None
-                else nullcontext() as output_file,
-        ):
-            # Define voltage output channels for coil control ([X, Y])
-            task_o.ao_channels.add_ao_voltage_chan(config.COILS_NAME_OX)
-            task_o.ao_channels.add_ao_voltage_chan(config.COILS_NAME_OY)
-
-            # Set maximum permittable voltage for output channels
-            task_o.ao_channels.all.ao_max = config.COILS_MAX_CURRENT
-            task_o.ao_channels.all.ao_min = -config.COILS_MAX_CURRENT
-
-            if output_file is not None:
-                out_writer = csv.writer(output_file)
-                out_writer.writerow(['Time', 'B_x', 'B_y'])
-
+        with nidaqmx.Task() if self.initialized else nullcontext() as task_o:
             # Waveform generation loop
-            while not self.stopped():
+            while not self._stopper.is_set():
                 time_now = time.time_ns() * 1e-9
 
                 # Set coil voltages (effectively, coil current)
+                self._field = {
+                    't': time_now,
+                    'x': coils.waveform.sine(
+                        time_now,
+                        self.params['x_amp'],
+                        self.params['x_freq'],
+                        self.params['x_phase'],
+                    ),
+                    'y': coils.waveform.sine(
+                        time_now,
+                        self.params['y_amp'],
+                        self.params['y_freq'],
+                        self.params['y_phase'],
+                    ),
+                }
                 sent_t = np.array([
-                    coils.waveform.sine(
-                        time_now, self.amp, self.freq, 0),
-                    coils.waveform.sine(
-                        time_now, self.amp, self.freq, self.phase),
+                    self._field['x'],
+                    self._field['y'],
                 ])
-                task_o.write(
-                    sent_t * np.array([
-                        config.COILS_T_TO_V_X,
-                        config.COILS_T_TO_V_Y,
-                    ])
-                )
-
-                # Write coil voltages
-                if output_file is not None:
-                    out_writer.writerow(np.concatenate([time_now], sent_t))
+                if task_o is not None:
+                    task_o.write(
+                        sent_t * np.array([
+                            config.COILS_T_TO_V_X,
+                            config.COILS_T_TO_V_Y,
+                        ])
+                    )
 
                 # Limit frame rate
                 time.sleep(max(
@@ -90,4 +101,5 @@ class Coils(threading.Thread):
                 ))
 
             # Set current through coils to zero upon exit
-            task_o.write([0, 0])
+            if task_o is not None:
+                task_o.write([0, 0])
--- visual/camera.py
+++ visual/camera.py
@@ -1,41 +1,57 @@
+import time
+
+import numpy as np
 from pypylon import pylon
 
 
 class Camera():
-    def __init__(self, fps: int, exposure_time: int):
-        self.fps = fps
-        self.exposure_time = exposure_time
+    def __init__(self):
+        self.exposure_time = None
 
-        # Connect to the first available camera
-        self.camera = pylon.InstantCamera(
-            pylon.TlFactory.GetInstance().CreateFirstDevice())
-        self.camera.Open()
-        self.camera.ExposureTime.SetValue(self.exposure_time)
-        self.camera.Close()
+        # Calibration parameters
+        self.mtx = None
+        self.dist = None
+
+        self.camera = None
 
         # Image converter to cv2 format (bgr)
         self.converter = pylon.ImageFormatConverter()
         self.converter.OutputPixelFormat = pylon.PixelType_BGR8packed
         self.converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
 
-        # Calibration parameters
-        self.mtx = None
-        self.dist = None
-    
+    def initialize(self, exposure_time: int):
+        self.exposure_time = exposure_time
+        # Connect to the first available camera
+        try:
+            self.camera = pylon.InstantCamera(
+                pylon.TlFactory.GetInstance().CreateFirstDevice())
+            self.camera.Open()
+            self.camera.ExposureTime.SetValue(self.exposure_time)
+            self.camera.Close()
+        except Exception:
+            raise Exception("Camera is not found")
+
     def calibrate(self, mtx, dist):
         self.mtx = mtx
         self.dist = dist
 
     def start_capture(self):
-        self.camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
+        if self.camera is not None:
+            self.camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
 
     def stop_capture(self):
-        self.camera.StopGrabbing()
+        if self.camera is not None:
+            self.camera.StopGrabbing()
 
     def grab(self):
-        grabResult = self.camera.RetrieveResult(
-            5000, pylon.TimeoutHandling_ThrowException)
-        image = self.converter.Convert(grabResult)
-        img = image.GetArray()
-        grabResult.Release()
+        img = None
+        if self.camera is not None:
+            grabResult = self.camera.RetrieveResult(
+                5000, pylon.TimeoutHandling_ThrowException)
+            image = self.converter.Convert(grabResult)
+            img = image.GetArray()
+            grabResult.Release()
+        else:
+            img = np.ones((300, 500, 3)) * 255 / 8 * (2 + np.sin(time.time_ns() * 3.e-9))
+            img = img.astype(np.uint8)
         return img
