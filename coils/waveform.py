0e41b310d1559a5bb81e544d5069aa0387baecd4
Viesturs Spūlis <v.spuulis@gmail.com>
2022-09-01 20:17:54 +0300

    Renamed variables to be more explicit

--- coils/waveform.py
+++ coils/waveform.py
@@ -1,26 +1,26 @@
 import numpy as np
 
 
-def sine(t, args):
-    return args.get('amp', 0.) * np.sin(
-        args.get('freq', 0.) * t - args.get('phase', 0.))
+def sine(t, parameters):
+    return parameters.get('amp', 0.) * np.sin(
+        parameters.get('freq', 0.) * t - parameters.get('phase', 0.))
 
 
-def triangle(t, args):
-    return 2 * args.get('amp', 0.) / np.pi * np.arcsin(np.sin(
-        args.get('freq', 0.) * t - args.get('phase', 0.)))
+def triangle(t, parameters):
+    return 2 * parameters.get('amp', 0.) / np.pi * np.arcsin(np.sin(
+        parameters.get('freq', 0.) * t - parameters.get('phase', 0.)))
 
 
-def sawtooth(t, args):
-    return 2 * args.get('amp', 0.) / np.pi * np.arctan(np.tan(
-        args.get('freq', 0.) * t - args.get('phase', 0.)))
+def sawtooth(t, parameters):
+    return 2 * parameters.get('amp', 0.) / np.pi * np.arctan(np.tan(
+        parameters.get('freq', 0.) * t - parameters.get('phase', 0.)))
 
 
 class Waveform():
     def __init__(self):
         # Function arguments and function
-        self._prms = {'x': {}, 'y': {}}
-        self.func = lambda t, args: 0
+        self.parameters = {'x': {}, 'y': {}}
+        self._function = lambda t, args: 0
         self.profile = {
             'ts': [],
             'xs': [],
@@ -30,29 +30,29 @@ class Waveform():
     def generate(self, t):
         return {
             't': t,
-            'x': self.func(t, self._prms['x']),
-            'y': self.func(t, self._prms['y']),
+            'x': self._function(t, self.parameters['x']),
+            'y': self._function(t, self.parameters['y']),
         }
 
-    def set_function(self, func):
-        self.func = func
+    def set_function(self, _function):
+        self._function = _function
         self.generate_profile()
 
-    def update_parameters(self, new_prms, override=False):
+    def update_parameters(self, new_parameters, override=False):
         if override:
             self.clear_parameters()
-        if 'x' in new_prms:
-            self._prms['x'].update(new_prms['x'])
-        if 'y' in new_prms:
-            self._prms['y'].update(new_prms['y'])
+        if 'x' in new_parameters:
+            self.parameters['x'].update(new_parameters['x'])
+        if 'y' in new_parameters:
+            self.parameters['y'].update(new_parameters['y'])
         self.generate_profile()
 
     def clear_parameters(self):
-        self._prms = {'x': {}, 'y': {}}
+        self.parameters = {'x': {}, 'y': {}}
 
     def generate_profile(self, length=3., dt=0.006):
         ts = np.arange(0, length, dt)
-        xs = self.func(ts, self._prms['x'])
-        ys = self.func(ts, self._prms['y'])
+        xs = self._function(ts, self.parameters['x'])
+        ys = self._function(ts, self.parameters['y'])
         self.profile = {'ts': ts, 'xs': xs, 'ys': ys}
         return self.profile
